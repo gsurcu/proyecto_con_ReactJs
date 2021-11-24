4012d4a183f5b59e836b8dca5c7c8a19b990c0a7
Gabriel Surculento <gespejo121@gmail.com>
2021-11-24 11:22:04 -0300

    Modificando Routes

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,48 +1,36 @@
 import React, {useState, useEffect} from "react";
-// import { ItemCount } from "../ItemCount/ItemCount";
-// import { ItemList } from "../ItemList/ItemList";
+import { pedirDatos } from "../../helpers/pedirDatos";
+import { ItemList } from "../ItemList/ItemList";
 
 
 export const ItemListContainer = () => {
 	
 	const [loading, setLoading] = useState(false)
-
-	const pedirDatos = () => {
-		return new Promise((resolve,reject) => {
-			
-			setTimeout(() => {
-				resolve("Promesa resuelta")
-			}, 2000)
-		})
-	}
+	const [productos, setProductos] = useState([])
 
 	useEffect(() => {
 
 		setLoading(true)
 		pedirDatos()
-		.then( (resp) => {
-			console.log(resp)
-		})
-		.catch( (error) => {
-			console.log(error)
-		})
-		.finally(() => {
-			setLoading(false)
-		})
+			.then( (resp) => {
+				setProductos(resp)
+			})
+			.catch( (error) => {
+				console.log(error)
+			})
+			.finally(() => {
+				setLoading(false)
+			})
 		
 	}, [])
 
 	return (
-		<div>
-			<h2>Proyecto con ReactJS</h2>
-			{/* <ItemCount /> */}
-			{/* <ItemList /> */}
-
+		<>
 			{
 				loading 
 				? <h2>Cargando...</h2> 
-				: <h2>Productos Listos</h2>
+				: <ItemList items={productos}/>
 			}
-		</div>
+		</>
 	)
 }
